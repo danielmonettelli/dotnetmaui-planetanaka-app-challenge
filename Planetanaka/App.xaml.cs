1b105635bf526659b036a23f088d977d87f3e35d
Daniel Monettelli <danielmonetelli@hotmail.com>
2023-04-18 09:33:00 -0500

    🎉 [Code] Added initial .NET MAUI template

---
+++ Planetanaka/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Planetanaka;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Planetanaka/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Planetanaka;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
